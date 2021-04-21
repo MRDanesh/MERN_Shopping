3edb2fe130199d3d4a325c08807b76f1e6d1ce65
MRDanesh <Mohammadrezad126@gmail.com>
2021-04-21 18:39:16 -0400

    more stylin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Header from './Header';
 import Footer from './Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 class App extends React.Component{
     render() {
         return (
-            <>
+            <Router>
                 <Header/>
                 <main className='py-3'>
                     <Container>
-                        <h1>
-                            Welcome To Shopping Center 
-                        </h1>
+                        <Route path = '/' component = {HomeScreen} exact  />
+                        <Route path = '/product/:id' component={ProductScreen} />
                     </Container>
                 </main>
                 <Footer/>
-            </>
+            </Router>
         );
     }
 }
